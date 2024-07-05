1b1c0f9775e9b016767c136ff876096bd64f34aa
Egrvigrf <engraveingrief@gmail.com>
2024-07-06 00:31:32 +0800

    Update 7-6

--- greedy_snake/snake.cpp
+++ greedy_snake/snake.cpp
@@ -1,302 +1,163 @@
-#include <iostream> //20231225 Snake2
+#include <iostream> 
 #include <random>
 #include <cstring>
 #include <conio.h> //isn't stl
 #include <chrono>  //sleep
 #include <thread>
+#include<limits>
+#include<map>
 using namespace std;
-struct link_snk
-{
+struct link_snk {
     int x, y;
-    link_snk *next = NULL;
+    link_snk* next = nullptr, * pre = nullptr;
 };
-class snk
-{
-public:
-    int speed, len;
-    char dir, snk_head_image, snk_body_image;
-    link_snk *head;
-    snk()
-    {
-        speed = 150;
-        len = 2;
-        snk_head_image = '@';
-        snk_body_image = '*';
-    }
-    void premove(int &, int &);
-    void move(int, int);
-    void eat(int, int);
-};
-int maxscore = 2;
-int score = 2;
-int rows, cols;
+const char head_image = '@';
+const char body_image = '*';
+const int speed = 100;
+const int rows = 20, cols = 40;
+bool Mp[rows + 1][cols + 1];
+int snk_len, maxlen = 2;
+char snk_dir;
+link_snk* snk_head, * snk_tail;
 int fruit_x, fruit_y;
 int money_x, money_y;
-string die_type;
 bool fruit_is_exist = true;
-bool money_is_exist = false;
-bool money_first_genetate = true;
-snk snake;
-snk *const snake_p = &snake;
+const char matchdir[4] = { 'd', 's', 'a', 'w' };
+map<char, char> collide = { {'a','d'},{'d','a'},{'w','s'},{'s','w'} };
 void game_init();
-bool crash(int, int, bool);
-;
 bool replay();
-void print_x_y(int y, int x, char c)
-{
-    cout << "\033[" << y << ";" << x << "H" << c;
-}
-void print_x_y(int y, int x, string c)
-{
-    cout << "\033[" << y << ";" << x << "H" << c;
-}
-void Snap(int t)
-{
-    chrono::milliseconds pauseTime(t);
-    this_thread::sleep_for(pauseTime);
-}
-void fruit_generate()
-{
-    do
-    {
-        fruit_x = rand() % (rows - 2) + 2;
-        fruit_y = rand() % (cols - 2) + 2;
-    } while (crash(fruit_x, fruit_y, false) and (money_is_exist ? (fruit_x != money_x and fruit_y != money_y) : true));
-    print_x_y(fruit_x, fruit_y, '.');
-    fruit_is_exist = true;
-    money_first_genetate = true;
-}
-void money_genetate()
-{
-    do
-    {
-        money_x = rand() % (rows - 2) + 2;
-        money_y = rand() % (cols - 2) + 2;
-    } while (crash(money_x, money_y, false) and money_x != fruit_x and money_y != fruit_y);
-    print_x_y(money_x, money_y, '$');
-    money_is_exist = true;
-}
-int main()
-{
-    do
-    {
+void print_x_y(int y, int x, char c) { cout << "\033[" << y << ";" << x << "H" << c; }
+void print_x_y(int y, int x, string c) { cout << "\033[" << y << ";" << x << "H" << c; }
+void Snap(int t) { chrono::milliseconds pauseTime(t); this_thread::sleep_for(pauseTime); }
+void fruit_generate();
+bool move();
+bool check(char);
+int main() {
+    do {
         game_init();
-        while (true)
-        {
-            if (!fruit_is_exist)
-                fruit_generate();
-            int h_x, h_y; // future coordinates of new head
-            snake.premove(h_x, h_y);
-            if (crash(h_x, h_y, true))
-                break;
-            else if (fruit_x == h_x and fruit_y == h_y)
-                snake.eat(h_x, h_y);
-            else if (money_is_exist and money_x == h_x and money_y == h_y)
-            {
-                score += 5;
-                cout << "\033[" << 2 << ";" << cols + 15 << "H" << score;
-                snake.move(h_x, h_y);
-            }
-            else
-                snake.move(h_x, h_y);
-            if (money_is_exist)
-            {
-                if (rand() % (snake.speed) == 1)
-                {
-                    money_is_exist = false;
-                    print_x_y(money_x, money_y, ' ');
-                }
-            }
-            else
-            {
-                if (snake.len % (rand() % 3 + 1) == 0 and rand() % ((rows + cols) / 3) == 1 and money_first_genetate)
-                {
-                    money_genetate();
-                    money_first_genetate = false;
-                }
-            }
-            Snap(snake.speed);
+        while (true) {
+            if (!fruit_is_exist) fruit_generate();
+            if (!move()) break;
+            Snap(speed);
         }
     } while (replay());
     return 0;
 }
-void snk::premove(int &newhead_x, int &newhead_y)
-{
-    if (kbhit())
-    {
+bool check(char s) {
+    for(const char& t : matchdir) 
+        if(s == t) return true;
+    return false;
+}
+bool move() {
+    if (kbhit()) {
         char dir = getch(); // avoid direction collision
-        if (dir == 'a' || dir == 's' || dir == 'd' || dir == 'w')
-        {
-            if (dir == 'w' and snake_p->dir == 's')
-                dir = 's';
-            else if (dir == 's' and snake_p->dir == 'w')
-                dir = 'w';
-            else if (dir == 'a' and snake_p->dir == 'd')
-                dir = 'd';
-            else if (dir == 'd' and snake_p->dir == 'a')
-                dir = 'a';
-            snake_p->dir = dir;
+        if (check(dir) && collide[dir] == snk_dir) dir = collide[dir];
+        snk_dir = dir;
+    }
+    map<char, pair<int, int>> mp = { {'w',{-1,0}},{'s',{1,0}},{'a',{0,-1}},{'d',{0,1}} };
+    int newhead_x = snk_head->x + mp[snk_dir].first, newhead_y = snk_head->y + mp[snk_dir].second;
+    if (Mp[newhead_x][newhead_y]) {
+        return false;
+    } else {
+        print_x_y(newhead_x, newhead_y, head_image);
+        Mp[newhead_x][newhead_y] = 1;
+        print_x_y(snk_head->x, snk_head->y, body_image);
+        link_snk* p = new link_snk;
+        p->x = newhead_x;
+        p->y = newhead_y;
+        p->next = snk_head;
+        p->pre = nullptr;
+        snk_head->pre = p;
+        snk_head = p;
+        if (fruit_x == newhead_x and fruit_y == newhead_y) {
+            fruit_is_exist = false;
+            snk_len++;
+        } else {
+            print_x_y(snk_tail->x, snk_tail->y, ' ');
+            Mp[snk_tail->x][snk_tail->y] = 0;
+            snk_tail = snk_tail->pre;
+            delete snk_tail->next;       // delete tail
+            snk_tail->next = nullptr;
         }
     }
-    switch (snake_p->dir)
-    {
-    case 'w':
-        newhead_x = (snake_p->head)->x - 1;
-        newhead_y = (snake_p->head)->y;
-        break;
-    case 's':
-        newhead_x = (snake_p->head)->x + 1;
-        newhead_y = (snake_p->head)->y;
-        break;
-    case 'a':
-        newhead_x = (snake_p->head)->x;
-        newhead_y = (snake_p->head)->y - 1;
-        break;
-    case 'd':
-        newhead_x = (snake_p->head)->x;
-        newhead_y = (snake_p->head)->y + 1;
-    }
+    return true;
 }
-void snk::move(int new_x, int new_y)
-{
-    link_snk *p = snake.head;
-    while ((p->next)->next != NULL)
-        p = p->next;
-    print_x_y((p->next)->x, (p->next)->y, ' ');
-    delete p->next;
-    p->next = NULL;   // delete tail
-    p = new link_snk; // change head
-    p->next = snake_p->head;
-    p->x = new_x;
-    p->y = new_y;
-    print_x_y(new_x, new_y, snake.snk_head_image);
-    print_x_y(snake_p->head->x, snake_p->head->y, snake.snk_body_image); // head image -> body image
-    snake_p->head = p;
-}
-void snk::eat(int new_x, int new_y)
-{
-    print_x_y(snake_p->head->x, snake_p->head->y, snake.snk_body_image);
-    link_snk *p = new link_snk;
-    p->x = new_x;
-    p->y = new_y;
-    p->next = snake_p->head;
-    snake.head = p;
-    print_x_y(new_x, new_y, snake.snk_head_image);
-    snake.len++;
-    score++;
-    fruit_is_exist = false;
-    cout << "\033[" << 2 << ";" << cols + 15 << "H" << score;
-}
-void game_init()
-{
+
+void game_init() {
     cout << "\x1b[2J"; // clear screen
     cout << "\e[?25l"; // hide cursor
-    srand(static_cast<unsigned int>(time(nullptr)));
-    snake.len = 2;
-    score = 2;
-    rows = 20; //>=7
-    cols = 20; //>=7
-    int randxy[4][2] = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
-    char matchdir[4] = {'d', 's', 'a', 'w'};
-    print_x_y(1, 1, string(cols, '#'));
-    for (int i = 2; i <= rows - 1; i++)
-    {
-        print_x_y(i, 1, '#');
-        print_x_y(i, cols, '#');
+    memset(Mp, 0, sizeof(Mp));//0 means empty
+    srand(static_cast<unsigned int>(time(NULL)));
+    snk_len = 2;
+    int randxy[4][2] = { {0, -1}, {-1, 0}, {0, 1}, {1, 0} };
+    for (int i = 1; i <= cols; i++) print_x_y(1, i, '#'), Mp[1][i] = 1;
+    for (int i = 2; i <= rows - 1; i++) {
+        for (int j = 1; j <= cols; j++) {
+            if (j == 1 || j == cols) {
+                print_x_y(i, j, '#');
+                Mp[i][j] = 1;
+            } else print_x_y(i, j, ' ');
+        }
     }
-    print_x_y(rows, 1, string(cols, '#'));
-    snake.head = new link_snk; // initailize snake
-    snake.head->x = rand() % (rows - 4) + 3;
-    snake.head->y = rand() % (cols - 4) + 3;
-    print_x_y(snake.head->x, snake.head->y, snake.snk_head_image);
-    link_snk *tail = new link_snk;
+    for (int i = 1; i <= cols; i++) print_x_y(rows, i, '#'), Mp[rows][i] = 1;
+    snk_head = new link_snk; // initailize snake
+    snk_head->x = rand() % (rows - 4) + 3;
+    snk_head->y = rand() % (cols - 4) + 3;
+    print_x_y(snk_head->x, snk_head->y, head_image);
+    snk_tail = new link_snk;
     int i = rand() % 4;
-    tail->x = snake.head->x + randxy[i][0];
-    tail->y = snake.head->y + randxy[i][1];
-    snake.dir = matchdir[i];
-    snake.head->next = tail;
-    print_x_y(tail->x, tail->y, snake.snk_body_image);
+    snk_tail->x = snk_head->x + randxy[i][0];
+    snk_tail->y = snk_head->y + randxy[i][1];
+    snk_dir = matchdir[i];
+    snk_head->next = snk_tail;
+    snk_tail->pre = snk_head;
+    print_x_y(snk_tail->x, snk_tail->y, body_image);
     fruit_generate(); // initailize fruit
-    print_x_y(1, cols + 1, "Tap 'a' or 'w' or 'd' or 's' to start");
+    print_x_y(rows+2, 1, "Tap 'a' or 'w' or 'd' or 's' to start");
     bool is_start = false;
-    while (!is_start)
-    {
-        for (int i = cols + 1; i <= cols + 5; i++)
-        {
-            if (kbhit())
-            {
+    while (!is_start) {
+        for (int i =  1; i <= 5; i++) {
+            if (kbhit()) {
                 char dir = getch(); // avoid direction collision
-                if (dir == 'a' || dir == 's' || dir == 'd' || dir == 'w')
-                {
-                    if (dir == 'w' and snake_p->dir == 's')
-                        continue;
-                    else if (dir == 's' and snake_p->dir == 'w')
-                        continue;
-                    else if (dir == 'a' and snake_p->dir == 'd')
-                        continue;
-                    else if (dir == 'd' and snake_p->dir == 'a')
-                        continue;
-                    snake_p->dir = dir;
+                if (check(dir) && collide[dir] != snk_dir) {
+                    snk_dir = dir;
                     is_start = true;
                     break;
                 }
             }
-            print_x_y(2, i, '.');
+            print_x_y(rows + 3, i, '.');
             Snap(200);
         }
-        print_x_y(2, cols + 1, "     ");
+        print_x_y(rows + 3, 1, "     ");
     }
-    print_x_y(1, cols + 1, "                                     ");
-    print_x_y(1, cols + 1, "History record:");
-    cout << maxscore << endl;
-    print_x_y(2, cols + 1, "Current score:2");
+    print_x_y(rows + 3, 1, "     ");
+    print_x_y(rows + 2, 1, "                                     ");
+    print_x_y(rows + 2, 1, "History Record:");
+    cout << maxlen << endl;
 }
-bool crash(int x, int y, bool snk_crash)
-{
-    link_snk *p = snake.head;
-    if ((x == 1) || (x == rows) || (y == 1) || (y == cols))
-    {
-        if (snk_crash)
-            die_type = "You crash the wall!";
-        return true;
-    }
-    while (p->next != NULL)
-    {
-        if ((x == p->x) & (y == p->y))
-        {
-            if (snk_crash)
-                die_type = "You crash yourself!";
-            return true;
-        }
-        p = p->next;
-    }
-    if ((x == p->x) & (y == p->y))
-    {
-        if (snk_crash)
-            die_type = "You crash yourself!";
-        return true;
-    }
-    return false;
+
+void fruit_generate() {
+    do {
+        fruit_x = rand() % (rows - 2) + 2;
+        fruit_y = rand() % (cols - 2) + 2;
+    } while (Mp[fruit_x][fruit_y]);
+    print_x_y(fruit_x, fruit_y, '.');
+    fruit_is_exist = true;
 }
-bool replay()
-{
+
+bool replay() {
     cout << "\x1b[2J"; // clear screen
-    print_x_y(1, 1, "Game over! " + die_type);
-    cout << "\nLen:" << snake.len << " Score:" << score;
-    if (score > maxscore)
-    {
-        maxscore = score;
-        cout << " Congratulations! You break the record." << endl;
-    }
-    else
-        cout << endl;
+    print_x_y(1, 1, "Game over! \n");
+    cout << "length:" << snk_len;
+    if (snk_len > maxlen) {
+        maxlen = snk_len;
+        cout << " \nCongratulations! You break the record.";
+    } 
+    cout << endl;
     cout << "Tap ant key to replay or 'q' for quitting";
-    while (true)
-    {
-        for (int i = 1; i <= 5; i++)
-        {
-            if (kbhit())
-            {
+    while (true) {
+        for (int i = 1; i <= 5; i++) {
+            if (kbhit()) {
                 char re = getch();
                 if (re == 'q')
                     return false;
